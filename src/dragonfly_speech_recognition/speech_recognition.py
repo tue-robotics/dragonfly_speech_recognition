2d1887b75b1458b1753e68bd673f04ef4225b39e
Rein Appeldoorn <reinzor@gmail.com>
2014-09-16 20:36:47 +0200

    Loading dragonfly

--- src/dragonfly_speech_recognition/speech_recognition.py
+++ src/dragonfly_speech_recognition/speech_recognition.py
@@ -1,1 +1,23 @@
-import 
+#!/usr/bin/env python
+import rospy
+import rospkg
+import sys, os
+
+rospack = rospkg.RosPack()
+
+def error(error):
+    print "ERROR: %s"%error
+    sys.exit()
+
+try:
+    dragonfly_path = "%s/deps/dragonfly"%rospack.get_path('dragonfly_speech_recognition')
+except:
+    error("Could not get package path from package dragonfly_speech_recognition")
+
+try:
+    sys.path.append(dragonfly_path)
+    import dragonfly
+except:
+    error("Failed to import dragonfly, path: %s"%dragonfly_path)
+
+print "[[[ Succesfully loaded dragonfly ]]]"
