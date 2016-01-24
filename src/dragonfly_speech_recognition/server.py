c67f8f465457cfa5f53c5f5081326583bc256eca
Ramon Wijnands <rayman747@hotmail.com>
2016-01-24 22:36:38 +0100

    refactor(server): inline class definition and better logging

--- src/dragonfly_speech_recognition/server.py
+++ src/dragonfly_speech_recognition/server.py
@@ -1,85 +1,120 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys
+import os
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
 
 if os.name is not 'nt':
-    print "This module should run on a windows machine :)"
-    sys.exit()
+    logger.error("This module should run on a windows machine :)")
+    sys.exit(1)
 
 import time
-import logging
 import pythoncom
 import winsound
 import socket
-
-# XML RPC SERVER
-from threading import Thread
-import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer as Server
+from Queue import Queue, Empty
+from collections import namedtuple
 
-def error(error):
-    print "ERROR: %s"%error
-    sys.exit()
-
-dragonfly_path = "%s/../../deps/dragonfly"%os.path.dirname(os.path.realpath(__file__))
-data_path = "%s/../../data"%os.path.dirname(os.path.realpath(__file__))
+dragonfly_path = "%s/../../deps/dragonfly" % os.path.dirname(os.path.realpath(__file__))
+data_path = "%s/../../data" % os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dragonfly_path)
 
 try:
     from dragonfly.engines.backend_sapi5.engine import Sapi5InProcEngine
     from dragonfly import (Grammar, CompoundRule, Dictation, Choice)
 except:
-    error("Failed to import dragonfly, path: %s"%dragonfly_path)
+    logger.error("Failed to import dragonfly, path: %s" % dragonfly_path)
+    sys.exit(-1)
 
-#---------------------------------------------------------------------------
 
-RESULT = None
+'''Internal struct to store speech results'''
+Result = namedtuple('Result', ['node', 'extras'])
 
-#---------------------------------------------------------------------------
 
-class GrammarRule(CompoundRule):   
-    def _process_recognition(self, node, extras):
-        global RESULT
-        RESULT = extras
 
-# RPC METHOD
 def recognize(spec, choices_values, timeout):
+    """
+    RPC callback that will call dragonfly
+    """
+    logger.info('RPC request: "%s", %s (timeout %d seconds)', spec, repr(choices_values), timeout)
 
-    # Flush waiting messages
-    pythoncom.PumpWaitingMessages()
-    global RESULT
-    RESULT = None
+    try:
+        result = dragonfly_recognise(spec=spec, choices_values=choices_values, timeout=timeout)
+    except Exception as e:
+        logger.exception('dragonfly exception:')
+        raise e
 
-    grammar = Grammar("grammar")
+    logger.info('Result: %s', repr(result))
+    return result
 
+
+def dragonfly_recognise(spec, choices_values, timeout):
+    """
+    Build a grammar based on spec and choices and send it to dragonfly
+    """
+
+    # build the dragonfly request
     extras = []
     for name, choices in choices_values.iteritems():
-        extras.append(Choice(name, dict((c,c) for c in choices)))
+        extras.append(Choice(name, dict((c, c) for c in choices)))
 
-    Rule = type("Rule", (GrammarRule,),{"spec": spec, "extras": extras})
-    grammar.add_rule(Rule())
+    results = Queue()
+
+    class GrammarRule(CompoundRule):
+        def _process_recognition(self, node, extras):
+            logger.info('_process_recognition callback: %s', str(node))
+            results.put_nowait(Result(node=node, extras=extras))
+
+        def _process_begin(self):
+            logger.debug('Rule:__process_begin')
+
+
+    rule = GrammarRule(spec=spec, extras=extras)
+
+    grammar = Grammar("grammar")
+    grammar.add_rule(rule)
+
+    # attach failure callback
+    def process_recognition_failure():
+        logger.info('Grammar:process_recognition_failure')
+    grammar.process_recognition_failure = process_recognition_failure
+
     grammar.load()
-
-    print "Grammar loaded"
     winsound.PlaySound(data_path + "/grammar_loaded.wav", winsound.SND_ASYNC)
 
+    logger.info("Grammar loaded: %s", spec)
+
     future = time.time() + timeout
-    while time.time() < future and RESULT is None:
+    while time.time() < future and results.empty():
         pythoncom.PumpWaitingMessages()
         time.sleep(.1)
 
     grammar.unload()
 
-    result = None
-    if RESULT:
-        result = {}
-        result["result"] = " ".join(RESULT["_node"].words())
-        result["choices"] = {}
-        for choice in choices_values:
-            if choice in RESULT:
-                result["choices"][choice] = RESULT[choice]
+    try:
+        result = results.get_nowait()
+    except Empty:
+        logger.info('No result, probably a timeout')
+        return False
 
-    return result
+    if not results.empty():
+        raise Exception('Multiple results received')
+
+    # filter all extras with _ because they are private
+    return {
+        "result": result.node.value(),
+        "choices": {k: v for (k, v) in result.extras.items() if not k.startswith('_')}
+    }
+
+
+def process_result():
+    pass
+
 
 if __name__ == "__main__":
     engine = Sapi5InProcEngine()
@@ -92,7 +127,7 @@ if __name__ == "__main__":
     server.register_function(recognize, 'recognize')
 
     engine.speak('Speak recognition active!')
-    print "Speak recognition active at %s:%d"%(address,port)
+    logger.info("Speak recognition active at %s:%d", address, port)
 
     server.serve_forever()
 
--- src/dragonfly_speech_recognition/test_client.py
+++ src/dragonfly_speech_recognition/test_client.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
-from xmlrpclib import ServerProxy
+from xmlrpclib import ServerProxy, Error, Fault
 import sys
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 class GetSpeechClient():
-
     def __init__(self, speech_server_ip):
-        self.sp  = ServerProxy("http://%s:8000"%speech_server_ip)
+        self.sp = ServerProxy("http://%s:8000" % speech_server_ip)
 
     def call(self, spec):
         # RPC request to server
-        return self.sp.recognize(spec, {}, 10)
+        logger.info('speech request: "%s"', spec)
+        try:
+            # return self.sp.recognize(spec, {}, 10)
+            return self.sp.recognize('my name is <name>', {'name': ['Ramon', 'Paul']}, 10)
+        except Fault as error:
+            logger.exception('RPC Fault (%d): %s', error.faultCode, error.faultString)
+            sys.exit(1)
+
 
 # Main function
 if __name__ == '__main__':
