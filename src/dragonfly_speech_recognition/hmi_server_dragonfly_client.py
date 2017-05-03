4438d493ae741560a06a1de76a8477774d4d7441
Ramon Wijnands <rayman747@hotmail.com>
2017-05-03 15:47:35 +0200

    Implement HMI server

--- src/dragonfly_speech_recognition/hmi_server_dragonfly_client.py
+++ src/dragonfly_speech_recognition/hmi_server_dragonfly_client.py
@@ -1,6 +1,6 @@
 import rospy
-import multiprocessing.connection
-from hmi_server.abstract_server import AbstractHMIServer, HMIResult
+from hmi import AbstractHMIServer, HMIResult
+from dragonfly_speech_recognition.dragonfly_client import DragonflyClient
 
 
 class HMIServerDragonflyClient(AbstractHMIServer):
@@ -13,25 +13,14 @@ class HMIServerDragonflyClient(AbstractHMIServer):
         super(HMIServerDragonflyClient, self).__init__(rospy.get_name())
         self._address = (rospy.get_param('~dragonfly_server_ip', 'localhost'),
                          rospy.get_param('~dragonfly_server_port', 3000))
-        rospy.loginfo('Testing connection to to server {}'.format(self._address))
-        conn = multiprocessing.connection.Client(self._address)
-        rospy.loginfo("Success")
-        conn.close()
 
-    def _determine_answer(self, description, grammar, is_preempt_requested):
-        # Set-up connection and set grammar
-        conn = multiprocessing.connection.Client(self._address)
-        conn.send(grammar)
+    def _determine_answer(self, description, grammar, target, is_preempt_requested):
+        c = DragonflyClient(*self._address)
+        result = c.recognize(grammar, target, is_preempt_requested)
 
-        rospy.loginfo('Waiting for result ..')
-        while not conn.poll(.1):
-            if is_preempt_requested():
-                rospy.loginfo('preempt requested')
-                return None
+        if result is None:
+            return None
 
-        sentence, semantics = conn.recv()
-
-        rospy.loginfo('Received result: {}, {}'.format(sentence, semantics))
-        conn.close()
+        semantics, sentence = result
 
         return HMIResult(sentence, semantics)
