d43d72de4d86e67f53301782b9d9d7dc27848aa1
PetervDooren <p.v.dooren@tue.nl>
2022-07-12 18:02:17 +0700

    on all calls

--- src/dragonfly_speech_recognition/dragonfly_client.py
+++ src/dragonfly_speech_recognition/dragonfly_client.py
@@ -21,37 +21,34 @@ class DragonflyClient:
         self._address = (ip, port)
         self._timeout = timeout
 
-    def _get_connection(self):
+    def recognize(self, grammar, target, is_preempt_requested=None):
+        grammar_parser = CFGParser.fromstring(grammar)
+        grammar_parser.verify(target)
+
         start = time.time()
         while True:
             try:
-                return multiprocessing.connection.Client(self._address)
+                logging.info("Sending the recognition command")
+                conn = multiprocessing.connection.Client(self._address)
+                conn.send((grammar, target))
+                while not conn.poll(.1):
+                    if is_preempt_requested and is_preempt_requested():
+                        conn.send(0)  # Send a cancel request
+                        return None
+
+                # Now wait for result
+                sentence = conn.recv()
+
+                logging.info("Dragonfly Client received sentence %s", sentence)
+
+                return grammar_parser.parse(target, sentence), sentence
             except OSError:
                 if time.time() - start > self._timeout:
                     raise
                 time.sleep(0.1)
 
-    def recognize(self, grammar, target, is_preempt_requested=None):
-        grammar_parser = CFGParser.fromstring(grammar)
-        grammar_parser.verify(target)
-
-        logging.info("Sending the recognition command")
-        conn = self._get_connection()
-        conn.send((grammar, target))
-        while not conn.poll(.1):
-            if is_preempt_requested and is_preempt_requested():
-                conn.send(0)  # Send a cancel request
-                return None
-
-        # Now wait for result
-        sentence = conn.recv()
-
-        logging.info("Dragonfly Client received sentence %s", sentence)
-
-        return grammar_parser.parse(target, sentence), sentence
-
     def restart_node(self):
         logging.info("Sending the restart command")
 
-        conn = self._get_connection()
+        conn = multiprocessing.connection.Client(self._address)
         conn.send('restart_node')
